ba7159bae7f97caa9a3a00c6d1103692454fcb30
JessM <jess.t.mulliken@gmail.com>
2022-10-27 17:57:16 -0400

    commit user model

---
+++ models/User.js
@@ -0,0 +1,43 @@
+const { Sequelize } = require('sequelize');
+const sequelize = require('sequelize')
+const sequalizeConnection = require('../config/sequelizeConnection');
+const bcrypt = require('bcrypt');
+
+const User = sequalizeConnection.define('usser', {
+
+    id: {
+        type: Sequelize.INTEGER,
+        primarykey: true,
+        autoIncrement:true,
+        allowNull: false,
+    },
+
+    username: {
+        type: Sequelize.toString,
+        allowNull: false,
+        validate: {
+            len:[3],
+        }
+    },
+
+    password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        valiadte: {
+            len: [5, 60]
+        }
+    }
+
+}, {
+    sequelize: sequalizeConnection,
+    timestamps: false,
+    freezeTableName: true,
+    modelName: 'users',
+    underscore: ture
+});
+
+User.beforeCreate(async user => {
+    user.password = await bcrypt.hash(user.passworr, 10);
+})
+
+module.exports = User;
