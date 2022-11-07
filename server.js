77515d3551ce5ca86d878cbdeaff0413da29964e
JessM <jess.t.mulliken@gmail.com>
2022-11-07 13:20:47 -0500

    heroku push 2

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const express = require('express'):
+const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const helpers = require('./utils/helpers');
