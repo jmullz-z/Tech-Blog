429487e49f5ebf800a3f3b7eb6bc4903e6c55a50
JessM <jess.t.mulliken@gmail.com>
2022-11-07 13:28:53 -0500

    heroku push 5

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.use(express.json());
 app,use(express.urlencoded({ extended: flase }));
 app.use(express.static(path.join(_dirname, 'public')));
 
-// app,use(require('./controllers/'));
+app.use(require('./controllers/'));
 
 sequelize.sync({force: true}).then(() => {
     app.listen(PORT, () => console.log("Now Listening"))
