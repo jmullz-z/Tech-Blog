2105972d86e2a36d0e2e3ace60edef458b10f8cd
JessM <jess.t.mulliken@gmail.com>
2022-11-08 15:02:28 -0500

    Update: add heroku run script and updated server to print running port and url

--- server.js
+++ server.js
@@ -35,5 +35,5 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(routes);
 
 sequelize.sync({ force: true }).then(() => {
-  app.listen(PORT, () => console.log("Now Listening"));
+  app.listen(PORT, () => console.log(`Now Listening on port ${PORT}. View at http://localhost:${PORT}`));
 });
